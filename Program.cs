23721d7f6b01515e6b6306376f14c032b6784216
Yılmaz Yiğit Gökmen <gokmenyyigit@gmail.com>
2024-08-14 15:52:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+public class Program
+{
+    public static void Main(string[]args)
+    {
+        Console.WriteLine("** Davetliler **");
+
+        List<string> guests = new List<string>() {"1-Bülent Ersoy","2-Ajda Pekkan", "3-Ebru Gündeş",
+            "4-Hadise","5-Hande Yener", "6-Tarkan","7-Funda Arar","8-Demek Akalın"};
+
+        foreach (string s in guests)
+        {
+
+            Console.WriteLine(s);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
